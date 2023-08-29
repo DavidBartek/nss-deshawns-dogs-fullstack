1d25680fc08d10f9e676cc032dde2b091358a0a4
DavidBartek <davidwbartek@gmail.com>
2023-08-29 11:16:07 -0500

    initial commit

--- client/src/apiManager.js
+++ client/src/apiManager.js
@@ -1,4 +1,4 @@
 export const getGreeting = async () => {
-  const res = await fetch("/api/hello");
+  const res = await fetch("/api/hello"); // domain is omitted; results in an HTTP request to localhost:3000 due to proxy setting
   return res.json();
 };
